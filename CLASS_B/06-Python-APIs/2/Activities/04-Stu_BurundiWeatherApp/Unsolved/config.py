e6464a2c4753af769459ab87f1a5fe72fd2d3cf0
Mizanur Rahman <52867202+mizanur-rahman@users.noreply.github.com>
2023-09-21 23:34:02 -0400

    adding solutions

--- CLASS_B/06-Python-APIs/2/Activities/04-Stu_BurundiWeatherApp/Unsolved/config.py
+++ CLASS_B/06-Python-APIs/2/Activities/04-Stu_BurundiWeatherApp/Unsolved/config.py
@@ -1,2 +1,1 @@
-# Add your API key
-api_key = "YOUR KEY HERE!"
+api_key = "3a2f23c34d42afe6befaa3e51767745b"
--- CLASS_B/06-Python-APIs/2/Activities/05-Ins_OpenWeatherDataFrame/Unsolved/config.py
+++ CLASS_B/06-Python-APIs/2/Activities/05-Ins_OpenWeatherDataFrame/Unsolved/config.py
@@ -1,1 +1,1 @@
-api_key = "YOUR KEY HERE!"
+api_key = "3a2f23c34d42afe6befaa3e51767745b"
